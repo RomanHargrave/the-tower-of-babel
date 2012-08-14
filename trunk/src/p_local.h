4d7fd01d73d80337431ff7578ac771693f3d76b4
mcthevamp01@gmail.com <mcthevamp01@gmail.com@0eb28523-2a4b-4d5f-ebe6-796f958357e9>
2012-08-14 01:51:11 +0000

    Tower of Babel -------------- * Source now contains A_RadiusPull, as well as the below features. * Now there's code for A_PullActor(int force, int flags), which is intended for only one actor. Still pending change. * Implemented FDARI's newest A_CheckRay code.

--- trunk/src/p_local.h
+++ trunk/src/p_local.h
@@ -486,6 +486,10 @@ void	P_AimCamera (AActor *t1, fixed_t &x, fixed_t &y, fixed_t &z, sector_t *&sec
 void	P_RadiusAttack (AActor *spot, AActor *source, int damage, int distance, 
 						FName damageType, bool hurtSelf, bool dodamage=true, int fulldamagedistance=0, bool noimpactdamage=false);
 
+// [RC] Suction function.
+void	P_RadiusPull (AActor *pullspot, AActor *pullsource, int pullforce, int pulldistance,
+				   int fullpulldistance=0, int flags=0);
+
 void	P_DelSector_List();
 void	P_DelSeclist(msecnode_t *);							// phares 3/16/98
 void	P_CreateSecNodeList(AActor*,fixed_t,fixed_t);		// phares 3/14/98
